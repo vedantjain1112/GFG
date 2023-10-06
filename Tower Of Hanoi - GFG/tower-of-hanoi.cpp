5e99c885e378539e02159266783b5ba694ddc3bb
VEDANT JAIN <jainvedant1211@gmail.com>
2023-10-06 18:37:35 +0530

    Added solution - LeetHub

---
+++ Tower Of Hanoi - GFG/tower-of-hanoi.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+   long long toh(int N, int from, int to, int aux) {
+    if (N == 1) {
+        cout << "move disk 1 from rod " << from << " to rod " << to << endl;
+        return 1;
+    }
+
+    long long moves = 0;
+
+   
+    moves += toh(N - 1, from, aux, to);
+
+
+    cout << "move disk " << N << " from rod " << from << " to rod " << to << endl;
+    moves++;
+
+   
+    moves += toh(N - 1, aux, to, from);
+
+    return moves;
+}
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int T;
+    cin >> T;//testcases
+    while (T--) {
+        
+        int N;
+        cin >> N;//taking input N
+        
+        //calling toh() function
+        Solution ob;
+        
+        cout << ob.toh(N, 1, 3, 2) << endl;
+    }
+    return 0;
+}
+
+
+
+// } Driver Code Ends
