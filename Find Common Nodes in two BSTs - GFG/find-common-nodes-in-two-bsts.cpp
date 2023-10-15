5f14a1c426391937c64ab03bef02782b2cee2bc5
VEDANT JAIN <jainvedant1211@gmail.com>
2023-10-15 14:46:45 +0530

    Added solution - LeetHub

--- Find Common Nodes in two BSTs - GFG/find-common-nodes-in-two-bsts.cpp
+++ Find Common Nodes in two BSTs - GFG/find-common-nodes-in-two-bsts.cpp
@@ -85,10 +85,9 @@ Node* buildTree(string str)
 class Solution
 {
     public:
-    //Function to find the nodes that are common in both BST. 
-   vector<int> findCommon(Node *root1, Node *root2) {
+    vector<int> findCommon(Node* root1, Node* root2) {
         vector<int> result;
-        stack<Node *> stack1, stack2;
+        stack<Node*> stack1, stack2;
     
         while (root1 || root2 || !stack1.empty() || !stack2.empty()) {
             while (root1) {
@@ -102,32 +101,38 @@ class Solution
             }
     
             if (!stack1.empty() && !stack2.empty()) {
-                Node *top1 = stack1.top();
-                Node *top2 = stack2.top();
+                root1 = stack1.top();
+                root2 = stack2.top();
     
-                if (top1->data == top2->data) {
-                    result.push_back(top1->data);
+                if (root1->data == root2->data) {
+                    result.push_back(root1->data);
                     stack1.pop();
                     stack2.pop();
-                    root1 = top1->right;
-                    root2 = top2->right;
-                } else if (top1->data < top2->data) {
+                    root1 = root1->right;
+                    root2 = root2->right;
+                } else if (root1->data < root2->data) {
                     stack1.pop();
-                    root1 = top1->right;
+                    root1 = root1->right;
+                    root2 = nullptr;
                 } else {
                     stack2.pop();
-                    root2 = top2->right;
+                    root2 = root2->right;
+                    root1 = nullptr;
                 }
             } else if (!stack1.empty()) {
-                root1 = stack1.top()->right;
+                root1 = stack1.top();
                 stack1.pop();
+                root1 = root1->right;
+                root2 = nullptr;
             } else if (!stack2.empty()) {
-                root2 = stack2.top()->right;
+                root2 = stack2.top();
                 stack2.pop();
+                root2 = root2->right;
+                root1 = nullptr;
             }
         }
-
-         return result;
+    
+        return result;
     }
 
 };
