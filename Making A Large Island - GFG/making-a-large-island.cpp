7c3fa7202bfb3a9e30301884b87ab0e4af20e524
VEDANT JAIN <jainvedant1211@gmail.com>
2023-10-16 11:50:41 +0530

    Added solution - LeetHub

--- Making A Large Island - GFG/making-a-large-island.cpp
+++ Making A Large Island - GFG/making-a-large-island.cpp
@@ -8,61 +8,61 @@ class Solution
 {
 public:
       int largestIsland(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int result = 0;
-        int islandId = 2;
-
-        unordered_map<int, int> islandSizes;
-
-        function<int(int, int, int)> dfs = [&](int i, int j, int id) -> int {
-            if (i < 0 || i >= n || j < 0 || j >= n || grid[i][j] != 1) {
-                return 0;
-            }
-
-            grid[i][j] = id;
-            int size = 1;
-
-            size += dfs(i + 1, j, id);
-            size += dfs(i - 1, j, id);
-            size += dfs(i, j + 1, id);
-            size += dfs(i, j - 1, id);
-
-            return size;
-        };
-
-   
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (grid[i][j] == 1) {
-                    int size = dfs(i, j, islandId);
-                    islandSizes[islandId] = size;
-                    result = max(result, size);
-                    islandId++;
+            int n = grid.size();
+            int result = 0;
+            int islandId = 2;
+    
+            unordered_map<int, int> islandSizes;
+    
+            function<int(int, int, int)> dfs = [&](int i, int j, int id) -> int {
+                if (i < 0 || i >= n || j < 0 || j >= n || grid[i][j] != 1) {
+                    return 0;
                 }
-            }
-        }
-
-      
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (grid[i][j] == 0) {
-                    unordered_set<int> neighborIslands;
-                    int potentialSize = 1;
-
-                    
-                    if (i > 0) neighborIslands.insert(grid[i - 1][j]);
-                    if (i < n - 1) neighborIslands.insert(grid[i + 1][j]);
-                    if (j > 0) neighborIslands.insert(grid[i][j - 1]);
-                    if (j < n - 1) neighborIslands.insert(grid[i][j + 1]);
-
-                    for (int id : neighborIslands) {
-                        potentialSize += islandSizes[id];
+    
+                grid[i][j] = id;
+                int size = 1;
+    
+                size += dfs(i + 1, j, id);
+                size += dfs(i - 1, j, id);
+                size += dfs(i, j + 1, id);
+                size += dfs(i, j - 1, id);
+    
+                return size;
+            };
+    
+       
+            for (int i = 0; i < n; ++i) {
+                for (int j = 0; j < n; ++j) {
+                    if (grid[i][j] == 1) {
+                        int size = dfs(i, j, islandId);
+                        islandSizes[islandId] = size;
+                        result = max(result, size);
+                        islandId++;
                     }
-
-                    result = max(result, potentialSize);
                 }
             }
-        }
+    
+          
+            for (int i = 0; i < n; ++i) {
+                for (int j = 0; j < n; ++j) {
+                    if (grid[i][j] == 0) {
+                        unordered_set<int> neighborIslands;
+                        int potentialSize = 1;
+    
+                        
+                        if (i > 0) neighborIslands.insert(grid[i - 1][j]);
+                        if (i < n - 1) neighborIslands.insert(grid[i + 1][j]);
+                        if (j > 0) neighborIslands.insert(grid[i][j - 1]);
+                        if (j < n - 1) neighborIslands.insert(grid[i][j + 1]);
+    
+                        for (int id : neighborIslands) {
+                            potentialSize += islandSizes[id];
+                        }
+    
+                        result = max(result, potentialSize);
+                    }
+                }
+            }
 
         return result;
     }
