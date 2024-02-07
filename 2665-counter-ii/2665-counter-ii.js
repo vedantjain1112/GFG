476e602810f9fb1e1a7c9b7a00d7103e31ad3ab5
VEDANT JAIN <jainvedant1211@gmail.com>
2024-02-07 20:49:43 +0530

    Time: 70 ms (15.44%), Space: 51.1 MB (26.31%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,28 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let resetedValue = init;
+    let obj = {
+        increment: function() {
+            return ++init;
+        },
+        decrement: function() {
+            return --init;
+        },
+        reset: function() {
+            init = resetedValue;
+            return init;
+        }
+    };
+
+    return obj;
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
